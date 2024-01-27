a1f064dbe6af0f7375197c07000c3245c3d1438e
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-01-28 01:26:32 +0600

    Again (#3)
    * 1
    
    1
    
    * regPage
    
    regPage
    
    * loginPageBack
    
    ---------
    
    Co-authored-by: sunshine <69354535+sunsh1nezxc@users.noreply.github.com>

--- src/main/java/com/lifeinsuranceapplication/Main.java
+++ src/main/java/com/lifeinsuranceapplication/Main.java
@@ -2,6 +2,7 @@ package com.lifeinsuranceapplication;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 368, 736);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("LogInPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Halyk");
         stage.setScene(scene);
         stage.show();
     }
