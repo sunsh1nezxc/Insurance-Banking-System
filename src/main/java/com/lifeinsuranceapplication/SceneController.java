154c8c419a51365e12e8068eef011b62d38deaf0
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-01-28 02:23:23 +0600

    LoginLogicWorking

--- src/main/java/com/lifeinsuranceapplication/SceneController.java
+++ src/main/java/com/lifeinsuranceapplication/SceneController.java
@@ -1,10 +1,14 @@
 package com.lifeinsuranceapplication;
 
+import database.Database;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +18,12 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
 
     public void switchToSceneRegisterPage(ActionEvent event) throws IOException{
         root = FXMLLoader.load(getClass().getResource("RegisterPage.fxml"));
@@ -31,4 +41,7 @@ public class SceneController {
         stage.show();
     }
 
+    public void loginCheck(ActionEvent actionEvent) {
+        System.out.print(Database.checkLoginPassword(loginField.getText(), passwordField.getText()));
+    }
 }
