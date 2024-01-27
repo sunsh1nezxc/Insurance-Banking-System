be3429051cec4738070ee18ab9d16630e0108f3b
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-01-28 01:19:21 +0600

    Revert "Created DataBase and Login Check"
    This reverts commit 9560e8d4e19df952ce1f0744fe6cfa09e43bab81.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.lifeinsuranceapplication {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.xerial.sqlitejdbc;
 
 
     opens com.lifeinsuranceapplication to javafx.fxml;
